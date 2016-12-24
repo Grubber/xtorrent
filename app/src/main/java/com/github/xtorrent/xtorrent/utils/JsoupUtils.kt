00605f82cc186b72b53a3eded9422a3ba0de2f96
Grubber <grubber9527@gmail.com>
2016-12-24 22:41:23 +0800

    修改 语言 cookie 设置问题

--- app/src/main/java/com/github/xtorrent/xtorrent/utils/JsoupUtils.kt
+++ app/src/main/java/com/github/xtorrent/xtorrent/utils/JsoupUtils.kt
@@ -2,15 +2,20 @@ package com.github.xtorrent.xtorrent.utils
 
 import org.jsoup.Connection
 import org.jsoup.Jsoup
-import java.util.*
 
 /**
  * Created by grubber on 2016/12/24.
  */
 const val LANG_ZH = "zh"
+const val LANG_ZH_CN = "zh-cn"
+const val LANG_ZH_TW = "zh-tw"
 const val LANG_EN = "en"
 
 fun newJsoupConnection(url: String): Connection {
-    val locale = if (LANG_ZH == Locale.getDefault().language) LANG_ZH else LANG_EN
-    return Jsoup.connect(url).cookie("locale", locale)
+//    val locale = when (Locale.getDefault().language) {
+//        LANG_ZH -> LANG_ZH_CN
+//        else -> LANG_EN
+//    }
+//    return Jsoup.connect(url).cookie("locale", locale)
+    return Jsoup.connect(url)
 }
